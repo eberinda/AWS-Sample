a8d70d0c4dd36b748232a33ed3d33d4f07b88db2
Eugene Berinda <berinda.eg@gmail.com>
2025-02-24 09:29:54 +0100

    Restructured project added docker

--- AWS.Sample/AWS.Sample.API/Infrastructure/ServiceConfiguration.cs
+++ AWS.Sample/AWS.Sample.API/Infrastructure/ServiceConfiguration.cs
@@ -8,6 +8,7 @@ public static class ServiceConfiguration
     {
         Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(configuration)
+            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
             .CreateLogger();
 
         return services;
